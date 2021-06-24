34d7ef9a5c2d822f0d2adc7d5c96e8e916e41cbe
Camille <xdghost.kamil@gmail.com>
2021-06-25 00:25:12 +0300

    Initial Commit

---
+++ Tranzlator/TranzlatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranzlatorApp.swift
+//  Tranzlator
+//
+//  Created by Camille Khubbetdinov on 625..2021.
+//
+
+import SwiftUI
+
+@main
+struct TranzlatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
